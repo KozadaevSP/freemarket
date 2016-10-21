e94ac50d3447b5c8b7114a86607cfae677947abc
Scolopendromorph <Serge1.2007@mail.ru>
2016-10-21 17:46:47 +0300

    Delete myAdverts.php

--- cabinetUser/myAdverts.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include "dbConfig.php";
-
-$stmt = $pdo->prepare("SELECT * FROM adverts WHERE userId = :id");
-$stmt->bindParam(':id', $_COOKIE["userId"]);
-$stmt->execute();
-$result = $stmt->fetchAll((PDO::FETCH_ASSOC)); 
