89d01a72e9dee6587522bddefeace15eb3cd69f3
Scolopendromorph <Serge1.2007@mail.ru>
2016-10-21 17:47:03 +0300

    Delete personalInfo.php

--- cabinetUser/personalInfo.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include "dbConfig.php";
-
-$stmt = $pdo->prepare("SELECT * FROM users WHERE id = :id");
-$stmt->bindParam(':id', $_COOKIE["userId"]);
-$stmt->execute();
-$result = $stmt->fetch((PDO::FETCH_ASSOC)); 
